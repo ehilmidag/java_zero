8e8ce07cdc02af5fecc957dad9724193ce0f4f86
hilmidag <ehilmidag@gmail.com>
2023-09-02 04:53:34 +0300

    first commit

---
+++ hello_java/Hello.java
@@ -0,0 +1,14 @@
+public class Hello{
+    String world = "world";
+
+    public String sayHi(String who) {
+        String sentence;
+        if(who != "")
+            sentence = "hello " + who;
+        else
+            sentence = "hello " + world;
+
+        if
+        return sentence;
+    }
+}
---
+++ hello_java/HelloTest.java
@@ -0,0 +1,8 @@
+public class HelloTest{
+    public static void main(String[] args) {
+        Hello object = new Hello();
+        String answer = object.sayHi("hiltim");
+        System.out.println(answer);
+        
+    }
+}
